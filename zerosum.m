a93ec5c152ef10cc62a1a7c8fbb9c5ea59e7df86
gxy135 <3125912228@qq.com>
2025-01-09 18:21:42 +0800

    Add files via upload

--- zerosum.m
+++ zerosum.m
@@ -1,6 +1,6 @@
-%   如果没有纯策略解，您将获得混合策略解；否则，将显示纯策略解的索引。
+%   如果没有纯策略解，将获得混合策略解；否则，将显示纯策略解的索引。
 %
-% 此代码将零和博弈问题转换为线性规划问题，然后使用优化工具箱中的 linprog 来解决问题。
+% 将零和博弈问题转换为线性规划问题，然后使用优化工具箱中的 linprog 来解决问题。
 
 % 清空工作区和命令窗口
 clear all;
@@ -25,7 +25,9 @@ if min(max(A)) == max(min(A'))
     % 遍历每一列，找到最小值所在的行
     for i = 1:n
         for j = 1:m
+            % 如果当前列的最大值等于矩阵 A 中第 j 行第 i 列的元素
             if isequal(b(i), A(j, i))
+                % 并且该元素等于第 j 行的最小值
                 if isequal(A(j, i), min(A(j, :)))
                     r(length(r) + 1) = j; % 记录行索引
                     s(length(s) + 1) = i; % 记录列索引
@@ -35,35 +37,32 @@ if min(max(A)) == max(min(A'))
     end
     % 如果存在唯一的鞍点
     if (length(r) == 1 && length(s) == 1)
+        % 显示鞍点的位置和游戏值
         Answer = ['游戏在位置 (' int2str(r) ',' int2str(s) ') 处有一个鞍点，游戏的值为 ' num2str(A(r, s), 6) '。因此不需要混合策略。']
     else
         % 如果存在多个鞍点
         for i = 1:length(r)
             ms = [ms '(' int2str(r(i)) ',' int2str(s(i)) '),'];
         end
+        % 显示所有鞍点的位置和游戏值
         Answer = ['游戏在以下位置有鞍点：' ms ' 游戏的值为 ' num2str(A(r(1), s(1)), 6) '。因此不需要混合策略。']
     end
 else
     % 如果不存在鞍点，则使用线性规划求解混合策略
-    % 求解玩家 1 的混合策略
-    f = -[1; zeros(m, 1)]; % 目标函数系数
-    A_ineq = [ones(n, 1) -A']; % 不等式约束矩阵
-    b_ineq = zeros(n, 1); % 不等式约束向量
-    A_eq = [0 ones(1, m)]; % 等式约束矩阵
-    b_eq = 1; % 等式约束向量
-    lb = [-inf; zeros(m, 1)]; % 变量下界
-    % 调用 linprog 求解线性规划问题
-    X_a = linprog(f, A_ineq, b_ineq, A_eq, b_eq, lb);
+    % 玩家 1 的混合策略
+    % 目标函数为最小化 v
+    % 不等式约束为 A' * x >= v
+    % 等式约束为 sum(x) == 1
+    % 变量下界为 -inf 和 0
+    X_a = linprog(-[1; zeros(m, 1)], [ones(n, 1) -A'], zeros(n, 1), [0 ones(1, m)], [1], [-inf; zeros(m, 1)]);
     v = X_a(1, 1); % 提取游戏值
     X_a(1, :) = []; % 去除游戏值，得到混合策略
-    % 求解玩家 2 的混合策略
-    f = [1; zeros(n, 1)];
-    A_ineq = [-ones(m, 1) A];
-    b_ineq = zeros(m, 1);
-    A_eq = [0 ones(1, n)];
-    b_eq = 1;
-    lb = [-inf; zeros(n, 1)];
-    X_b = linprog(f, A_ineq, b_ineq, A_eq, b_eq, lb);
+    % 玩家 2 的混合策略
+    % 目标函数为最大化 v
+    % 不等式约束为 -A * y <= -v
+    % 等式约束为 sum(y) == 1
+    % 变量下界为 -inf 和 0
+    X_b = linprog([1; zeros(n, 1)], [-ones(m, 1) A], zeros(m, 1), [0 ones(1, n)], [1], [-inf; zeros(n, 1)]);
     X_b(1, :) = []; % 去除游戏值，得到混合策略
     % 显示结果
     Answer = ['游戏没有鞍点，游戏的值为 ' num2str(v, 6) '，因此建议的混合策略在混合策略矩阵中给出。']
